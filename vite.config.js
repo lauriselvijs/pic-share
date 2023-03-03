fd46c05dea59124e20332136378518502f12b631
Lauris <lauriselvijsm@gmail.com>
2023-03-03 13:08:51 +0200

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
     plugins: [
@@ -7,10 +8,11 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: "__tla",
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: (i) => `__tla_${i}`,
+        }),
     ],
-    resolve: {
-        alias: {
-            $: "jQuery",
-        },
-    },
 });
