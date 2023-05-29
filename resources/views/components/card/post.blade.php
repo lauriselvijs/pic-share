6b12ead8575a64ed0a48772b4a7fe4e479551068
Lauris <lauriselvijsm@gmail.com>
2023-05-29 14:59:35 +0300

    minor fix

--- resources/views/components/card/post.blade.php
+++ resources/views/components/card/post.blade.php
@@ -1,11 +1,10 @@
 @props(['post'])
 
 <div
-    class='flex flex-col gap-2 bg-shadow text-white text-left hover:md:rounded-[32px] rounded-none shadow-md transition-all'>
+    class='flex flex-col gap-2 bg-shadow text-white text-left hover:md:rounded-[32px] rounded-none shadow-md transition-all overflow-hidden'>
     <a href={{ route('posts.show', $post->slug) }}>
-        <img decoding="async" loading="lazy" class='h-full w-full hover:md:rounded-t-[32px] rounded-none transition-all'
-            src={{ asset($post->image) }}
-        alt='Post' title='Post'>
+        <img decoding="async" loading="lazy" class='h-full w-full' src={{asset($post->image)}} alt='Post'
+        title='Post'>
         <div class="p-6 pb-24 md:pb-12 flex flex-col justify-center items-start">
             <h2 class='text-2xl font-bold  leading-snug pb-6'>
                 {{ $post->title }}
