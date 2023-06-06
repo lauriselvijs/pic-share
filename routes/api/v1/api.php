f679c762696c1bb00c3cb6a51e847ea36f91c6e5
Lauris Mačāns <85683069+lauriselvijs@users.noreply.github.com>
2023-06-06 16:10:37 +0000

    minor fix

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -4,15 +4,14 @@ use App\Http\Controllers\api\AdminController;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['throttle:api'])->group(function () {
+    Route::apiResource('/admins', AdminController::class);
+    Route::group(['prefix' => 'admins', 'as' => 'admins.'], function () {
+        Route::post('/delete-queue', [AdminController::class, 'queueForDeletion'])->name('queue-for-deletion');
+        Route::delete('/batch/{deleteKey}', [AdminController::class, 'deleteAdmins'])->name('delete-admins');
+    });
 
-// Route::middleware(['throttle:api'])->group(function () {
-Route::apiResource('/admins', AdminController::class);
-// })
-Route::group(['prefix' => 'admins', 'as' => 'admins.'], function () {
-    Route::post('/delete-queue', [AdminController::class, 'queueForDeletion'])->name('queue-for-deletion');
-    Route::delete('/batch/{deleteKey}', [AdminController::class, 'deleteAdmins'])->name('delete-admins');
-});
-
-Route::get('/batch/{batchId}', function (string $batchId) {
-    return Bus::findBatch($batchId);
+    Route::get('/batch/{batchId}', function (string $batchId) {
+        return Bus::findBatch($batchId);
+    });
 });
