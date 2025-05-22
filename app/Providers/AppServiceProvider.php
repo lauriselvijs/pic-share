0542ea5abadd7da389853c1039ce1b8329b45013
lauriselvijs <lauriselvijsm@gmail.com>
2025-05-22 21:38:03 +0300

    Remove HTTPS scheme enforcement for production in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,10 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if (config('app.env') == 'production') {
-            $url->forceScheme('https');
-        }
-
         // Optimization
         Model::preventLazyLoading(! $this->app->isProduction());
 
